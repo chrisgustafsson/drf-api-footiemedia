0c1672ae389bc7a09c632130f99867d75869a809
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-15 18:43:48 +0000

    FIX error with cloudinary url settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -16,9 +16,9 @@ import os
 if os.path.exists('env.py'):
     import env
 
-CLOUDINARY_STORAGE = (
+CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
-)
+}
 MEDIA_URL = '/media'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
