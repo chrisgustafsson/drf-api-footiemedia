6116155f14b9a6d08e29c7abfcda4bef065550d6
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-17 23:02:24 +0000

    ADD postfields

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
@@ -7,7 +8,18 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
