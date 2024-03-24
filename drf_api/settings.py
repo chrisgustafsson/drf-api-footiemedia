517c2ed428fe54d18343ac6d4dce4054c4e2cdc1
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-24 22:58:15 +0000

    FIX url cors allowed origins

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -107,7 +107,7 @@ MIDDLEWARE = [
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
-        'https://3000-chrisgustafsson-footieme-35aoop9pgs.us1.codeanyapp.com',
+        'https://3000-chrisgustafsson-footieme-35aoop9pgs.us1.codeanyapp.com/',
     ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
