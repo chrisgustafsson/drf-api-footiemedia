e52d11131fededc186a5cea243c50eabf812095e
Christoffer Gustafsson <christoffer.gustafsson.cg@gmail.com>
2024-04-05 17:54:54 +0000

    ADD allowed origins

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -111,7 +111,7 @@ MIDDLEWARE = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN', 'https://footiemedia-pp5-31598719feb2.herokuapp.com'),
     ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
