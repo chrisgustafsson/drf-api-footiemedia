fdbb90af26925fb4de96c91c28be0b422048cdb5
Christoffer Gustafsson <christoffer.gustafsson.cg@gmail.com>
2024-07-25 16:21:09 +0000

    ADD dev.server to client origin dev

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,7 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '8000-chrisgustaf-drfapifooti-d
 CORS_ALLOW_HEADERS = list(default_headers)
 CORS_ALLOW_METHODS = list(default_methods)
 
-CLIENT_ORIGIN_DEV = os.environ.get('CLIENT_ORIGIN_DEV')
+CLIENT_ORIGIN_DEV = os.environ.get('CLIENT_ORIGIN_DEV', 'https://3000-chrisgustaf-footiemedia-izznfayr9op.ws-eu115.gitpod.io/')
 CLIENT_ORIGIN = os.environ.get('CLIENT_ORIGIN', 'https://footiemedia-pp5-31598719feb2.herokuapp.com')
 
 CSRF_TRUSTED_ORIGINS = [CLIENT_ORIGIN_DEV, CLIENT_ORIGIN] if CLIENT_ORIGIN_DEV else [CLIENT_ORIGIN]
